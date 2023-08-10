b3b4d92781f4a67d9bf2cf077364833790a48f1b
Gabriel <gabrielkellersouza@gmail.com>
2023-08-10 13:58:11 -0300

    Firts Commit - Add Codes and HTML

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # send_emails_with_python
+
+
+Tive um problema que estava ocorrendo porque o método .format() estava sendo aplicado ao conteúdo do template HTML, o que causava conflitos de substituição das chaves {} que fazem parte do código CSS. Como o CSS também utiliza chaves {} para definir blocos de estilo, o método .format() estava interpretando essas chaves como placeholders para formatação de string, o que gerava o erro.
+
+Ao remover o uso do método .format() e definir diretamente o corpo do email como o conteúdo do template HTML, consegui evitar esse conflito e permitiu que o HTML fosse enviado sem alterações indesejadas ou erros de execução.
+
+
+pip install python-decouple
---
+++ send_emails_with_python.py
@@ -0,0 +1,49 @@
+import smtplib
+import threading
+import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from decouple import config
+
+
+def send_email_erro_api_image(user_email, user_name, tempo_envio):
+    def enviar():
+        time.sleep(tempo_envio)
+
+        subject = 'Envio de email teste'
+        from_email = config('EMAIL_USER')
+        recipient_email = user_email
+
+        smtp_server = 'smtp.gmail.com'
+        smtp_port = 587
+        email_user = config('EMAIL_USER')
+        email_password = config('EMAIL_PASSWORD')
+
+        with open('email_template.html', 'r') as file:
+            html_template = file.read()
+
+        # Cria um objeto MIMEMultipart para o email
+        email = MIMEMultipart()
+        email['From'] = from_email
+        email['To'] = recipient_email
+        email['Subject'] = subject
+
+        # Define o corpo do email como o conteúdo do template HTML
+        email.attach(MIMEText(html_template, 'html'))
+
+        try:
+            server = smtplib.SMTP(smtp_server, smtp_port)
+            server.starttls()
+            server.login(email_user, email_password)
+            server.sendmail(from_email, recipient_email, email.as_string())
+            server.quit()
+        except Exception as e:
+            print("Erro ao enviar o email:", e)
+
+    # Cria uma thread para executar o envio
+    thread = threading.Thread(target=enviar)
+    thread.start()
+
+
+send_email_erro_api_image('exemple@gmail.com', 'Gub Teste', 5)
